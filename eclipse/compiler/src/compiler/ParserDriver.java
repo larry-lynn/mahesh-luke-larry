ead8239617073fe16284c8988737039ea4fbe674
Larry Lynn <larry@mars.tyob.info>
2013-02-12 21:13:47 -0700

    added tests for all my stubs.  its messy now, lots of hardcoding, needs cleanup

--- eclipse/compiler/src/compiler/ParserDriver.java
+++ eclipse/compiler/src/compiler/ParserDriver.java
@@ -18,8 +18,49 @@ public class ParserDriver {
         
         // XXX I think we'll get to this
         //parse.SystemGoal();
-        parse.StatementTail();
         
+        // Test 1 - use parse_input1.mp
+        //parse.StatementTail();
+        
+        // Test 2 - use parse_input1.mp -- again
+        //parse.EmptyStatement();
+        
+        // Test 3 - use parse_input2.mp
+        //parse.ReadParameterTail();
+        
+        // Test 4 - use parse_input3.mp
+        // parse.WriteStatement();
+        
+        // Test 5 - use parse_input4.mp
+        // parse.WriteParameterTail();
+        
+        // Test 6 - use parse_input5.mp
+        //parse.AssignmentStatement();
+        
+        // Test 7 - use parse_input6.mp
+        // parse.IfStatement();
+        
+        // Test 8 - use parse_input7.mp
+        //parse.OptionalElsePart();
+        
+        // Test 9 - use parse_input8.mp
+        //parse.WhileStatement();
+        
+        // Test 10 - use parse_input9.mp
+        // parse.ForStatement();
+        
+        // Test 11 - use parse_input10.mp
+        //parse.InitialValue();
+        
+        // Test 12 - use parse_input11.mp
+        //parse.StepValue();
+        //parse.FinalValue();
+        
+        // Test 13 - use parse_input5.mp -- again
+        //parse.ProcedureStatement();
+        
+        // Test 14 - use parse_input12.mp
+        parse.OptionalActualParameterList();
 
     } // end main()    
 	
